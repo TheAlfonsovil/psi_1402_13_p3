5c0c67fb2223e6eb6de3f0d51557caf7b8d3cea1
TheAlfonsovil <alfvilgar@gmail.com>
2019-11-24 19:26:20 +0100

    Cambios en views

--- ratonGato/logic/views.py
+++ ratonGato/logic/views.py
@@ -12,6 +12,9 @@ from django.http import HttpResponseForbidden
 from datamodel import constants
 from logic.forms import UserForm, UserRegister
 from django.contrib.auth import logout
+from datamodel.models import Game, GameStatus, Move
+from django.core.files import File
+import json
 
 # COMPROBAR AQUI COMO SON LAS FUNCIONES ->
 # https://psi1920-p3.herokuapp.com/mouse_cat/
@@ -89,34 +92,91 @@ def signup_service(request):
 
 
 def counter_service(request):
+	
     context_dict = {}
     return render(request,"mouse_cat/counter.html", context_dict)
 
-
+@login_required(redirect_field_name='',login_url='../login_service')
 def create_game_service(request):
-    context_dict = {}
+    game = Game(cat_user=request.user)
+    game.save()
+    context_dict = {'game': game}
     return render(request,"mouse_cat/new_game.html", context_dict)
 
-
+@login_required(redirect_field_name='',login_url='../login_service')
 def join_game_service(request):
-    context_dict = {}
-    return render(request,"join_game.html", context_dict)
+    games = Game.objects.order_by('id')
+    for game_aux in reversed(games):
+        if game_aux.status == 0:
+            game = game_aux #Created
+            break
+    game.mouse_user = request.user
+    game.status = 1 #Active
+    game.save()
+    context_dict = {'game': game}
+    return render(request,"mouse_cat/join_game.html", context_dict)
 
-
+@login_required(redirect_field_name='',login_url='../login_service')
 def select_game_service(request):
-    context_dict = {}
-    return render(request,"select_game.html", context_dict)
+    if request.method == 'GET':
+        games = Game.objects.order_by('id')
+        as_cat=[] #a lo mejor es []
+        for game in games:
+            if game.cat_user == request.user:
+                as_cat.add(game) # a lo mejor no se puede con add
+        as_mouse=[] #
+        for game in games:
+            if game.mouse_user == request.user:
+                as_mouse.add(game) #    
+        context_dict = {'as_cat': as_cat, 'as_mouse': as_mouse}
+        return render(request,"mouse_cat/select_game.html", context_dict)
 
+    else:
+        #POST -> hacer un showgame de juego seleccionado -> /select_game/241/  -> 241 es el id seleccionado
+        context_dict = {'as_cat': as_cat, 'as_mouse': as_mouse}
+        return render(request,"mouse_cat/game.html", context_dict)
 
+@login_required(redirect_field_name='',login_url='../login_service')
 def show_game_service(request):
-    context_dict = {}
-    return render(request,"game.html", context_dict)
+    if request.method == 'POST':
+        print(request.POST())
+    games = Game.objects.order_by('id')
+    gamef = {}
+    print(request.user)
+    print(request.user.id)
+    for game in games:
+        if game.cat_user.id == request.user.id:
+            gamef = game
+            break
 
+    board = [0] * 64
+    board[game.cat1] = 1
+    board[game.cat2] = 1
+    board[game.cat3] = 1
+    board[game.cat4] = 1
+    board[game.mouse] = -1
+    context_dict = {'game': gamef, 'board': board}
+    return render(request,"mouse_cat/game.html", context_dict)
+
+@login_required(redirect_field_name='',login_url='../login_service')
 def move_service(request):
-    context_dict = {}
-    return render(request,"game.html", context_dict)
-
-
+    if request.method == 'POST':
+        data = request.json()
+        print(data)
+        games = Game.objects.order_by('id')
+        for game in games:
+            if game.cat_user.id == request.user.id:
+	            gamef = game
+	            break
+        move = Move.objects.create(game=gamef, player=request.user, origin=request.POST[0], target=request.POST[1])
+        move.save()
+        context_dict = {'game': gamef}
+        return render(request,"mouse_cat/game.html", context_dict)
+    else:
+        context_dict = {}
+        return render(request,"mouse_cat/game.html", context_dict)
+#dos cadenas de caracteres con el nombre del usuario y su clav
+# jugador, juego, posici ́on inicial y final del movimient
 
 
 
